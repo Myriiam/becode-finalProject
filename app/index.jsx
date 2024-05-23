6c0d2423b5d1a1b7bb544c9d44848857faf4c251
Myriiam <kadiimyriam@hotmail.com>
2024-05-23 16:38:22 +0200

    main screens and tabs in react native with expo

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Helloo World ! My First React Native App</Text>
+    <View className="flex-1 items-center justify-center bg-white">      
+      <Text className="text-3xl">HomePage !</Text>
       <StatusBar style="auto" />
+      <Link href="/home" style={{ color: 'purple' }}>Go to Home</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-    container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    },
-});
